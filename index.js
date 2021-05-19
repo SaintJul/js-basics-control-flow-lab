1cf79e6e9af6008c2b041c74f75257f91282f5c0
SaintJul <favor.julius@gmail.com>
2021-05-18 22:52:24 -0400

    commit tree 19c64fa647147127d9c93bbb07c808011c9d06dc

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numFeet){
   // Write your code here!
+if (numFeet <= 400){
+  return 'This one is on me!';
+}else if (numFeet > 2000 && numFeet < 2500){
+  return 'I will gladly take your thirty bucks.';
+}else if (numFeet > 2500){
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const isCity = city === 'NYC' ? "Ok, sounds good.": "No go.";
+  return isCity
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+  let tipAmount;
+  switch(Tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous': 
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
+  
 }
